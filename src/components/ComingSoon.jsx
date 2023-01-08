6e71974c6e633faa9bf3a49a8d174c04f5f18a51
Yuvraaj Limbole <yuvrajl.103@gmail.com>
2023-01-08 19:24:31 +0530

    mobile no updated

--- src/components/ComingSoon.jsx
+++ src/components/ComingSoon.jsx
@@ -18,13 +18,13 @@ const CommingSoonNew = () => {
                 <div className="w-full py-4 border-t border-gray-300">
                     <div className="container mx-auto flex flex-col md:flex-row items-center justify-center text-gray-600 text-sm md:space-x-8 space-y-1 md:space-y-0">
                         <span className="font-bold">You can contact us:</span>
-                        <Link href="tel:+918329885941" className="flex items-center space-x-1" target="_blank" title="Call">
+                        <Link href="tel:+918408887137" className="flex items-center space-x-1" target="_blank" title="Call">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
                                 <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z"></path>
                             </svg>
                             <span>+91 840 888 7137</span>
                         </Link>
-                        <Link href="mailto:support@webinoxmedia.com" className="flex items-center space-x-1" target="_blank" title="Email">
+                        <Link href="mailto:support@24aryavrat.com" className="flex items-center space-x-1" target="_blank" title="Email">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
                                 <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
                                 <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
