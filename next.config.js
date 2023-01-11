80aaec6e889993100c9c9830968004d3361243ed
Yuvraj Limbole <yuvrajl.103@gmail.com>
2023-01-11 12:04:03 +0530

    Next-PWA configured

--- next.config.js
+++ next.config.js
@@ -1,9 +1,28 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  scope: '/',
+  sw: 'sw.js',
+  disable: false,
+  register: true,
+  skipWaiting: true,
+  fallbacks: {
+    image: '/24Aryavrat-logo.png',
+    // document: '/other-offline',  // if you want to fallback to a custom page other than /_offline
+    // font: '/static/font/fallback.woff2',
+    // audio: ...,
+    // video: ...,
+  },
+  cacheOnFrontEndNav: true,
+  reloadOnOnline: true,
+  // runtimeCaching,
+})
+
+const nextConfig = withPWA({
   env: {
-    API_URL: process.env.BACKEND_API,
-    BACKEND_BASE_URL: process.env.BACKEND_URL,
     BASE_URL: process.env.SITE_URL,
+    BACKEND_BASE_URL: process.env.BACKEND_URL,
+    API_URL: process.env.BACKEND_API,
   },
   reactStrictMode: true,
   swcMinify: true,
@@ -13,6 +32,6 @@ const nextConfig = {
     domains: ['images.unsplash.com', 'source.unsplash.com', 'dummyimage.com','cdn.tailgrids.com', 'fancytailwind.com'],
   },
   output: 'standalone',
-}
+})
 
 module.exports = nextConfig
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -21,7 +21,7 @@ export default function Document() {
         <meta name='msapplication-config' content='/browserconfig.xml' />
         <meta name='msapplication-TileColor' content='#023020' />
         <meta name='msapplication-tap-highlight' content='no' />
-        <meta name='theme-color' content='#000000' />
+        <meta name='theme-color' content='#ffffff' />
 
         <link rel='apple-touch-icon' href='/apple-touch-icon.png' />
         <link rel='apple-touch-icon' sizes='152x152' href='/icons/152x152.png' />
@@ -33,6 +33,42 @@ export default function Document() {
         <link rel='manifest' href='/manifest.json' />
         <link rel='mask-icon' href='/icons/512x512.png' color='#5bbad5' />
         <link rel='shortcut icon' href='/favicon.ico' />
+
+        {/* <!-- apple splash screen images --> */}
+        <link rel='apple-touch-startup-image' href='/splash_screens/4__iPhone_SE__iPod_touch_5th_generation_and_later_landscape.png' sizes='1136x640' />
+        <link rel='apple-touch-startup-image' href='/splash_screens/4__iPhone_SE__iPod_touch_5th_generation_and_later_portrait.png' sizes='640x1136' />
+        <link rel='apple-touch-startup-image' href='/splash_screens/8.3__iPad_Mini_landscape.png' sizes='2266x1488' />
+        <link rel='apple-touch-startup-image' href='/splash_screens/8.3__iPad_Mini_portrait.png' sizes='1488x2266' />
+        <link rel='apple-touch-startup-image' href='/splash_screens/9.7__iPad_Pro__7.9__iPad_mini__9.7__iPad_Air__9.7__iPad_landscape.png' sizes='2048x1536' />
+        <link rel='apple-touch-startup-image' href='/splash_screens/9.7__iPad_Pro__7.9__iPad_mini__9.7__iPad_Air__9.7__iPad_portrait.png' sizes='1536x2048' />
+        <link rel='apple-touch-startup-image' href='/splash_screens/10.2__iPad_landscape.png' sizes='2160x1620' />
+        <link rel='apple-touch-startup-image' href='/splash_screens/10.2__iPad_portrait.png' sizes='1620x2160' />
+        <link rel='apple-touch-startup-image' href='/splash_screens/10.5__iPad_Air_landscape.png' sizes='2224x1668' />
+        <link rel='apple-touch-startup-image' href='/splash_screens/10.5__iPad_Air_portrait.png' sizes='1668x2224' />
+        <link rel='apple-touch-startup-image' href='/splash_screens/10.9__iPad_Air_landscape.png' sizes='2360x1640' />
+        <link rel='apple-touch-startup-image' href='/splash_screens/10.9__iPad_Air_portrait.png' sizes='1640x2360' />
+        <link rel='apple-touch-startup-image' href='/splash_screens/11__iPad_Pro__10.5__iPad_Pro_landscape.png' sizes='2388x1668' />
+        <link rel='apple-touch-startup-image' href='/splash_screens/11__iPad_Pro__10.5__iPad_Pro_portrait.png' sizes='1668x2388' />
+        <link rel='apple-touch-startup-image' href='/splash_screens/12.9__iPad_Pro_landscape.png' sizes='2732x2048' />
+        <link rel='apple-touch-startup-image' href='/splash_screens/12.9__iPad_Pro_portrait.png' sizes='2048x2732' />
+        <link rel='apple-touch-startup-image' href='/splash_screens/iPhone_8__iPhone_7__iPhone_6s__iPhone_6__4.7__iPhone_SE_landscape.png' sizes='1334x750' />
+        <link rel='apple-touch-startup-image' href='/splash_screens/iPhone_8__iPhone_7__iPhone_6s__iPhone_6__4.7__iPhone_SE_portrait.png' sizes='750x1334' />
+        <link rel='apple-touch-startup-image' href='/splash_screens/iPhone_8_Plus__iPhone_7_Plus__iPhone_6s_Plus__iPhone_6_Plus_landscape.png' sizes='2208x1242' />
+        <link rel='apple-touch-startup-image' href='/splash_screens/iPhone_8_Plus__iPhone_7_Plus__iPhone_6s_Plus__iPhone_6_Plus_portrait.png' sizes='1242x2208' />
+        <link rel='apple-touch-startup-image' href='/splash_screens/iPhone_11__iPhone_XR_landscape.png' sizes='1792x828' />
+        <link rel='apple-touch-startup-image' href='/splash_screens/iPhone_11__iPhone_XR_portrait.png' sizes='828x1792' />
+        <link rel='apple-touch-startup-image' href='/splash_screens/iPhone_11_Pro_Max__iPhone_XS_Max_landscape.png' sizes='2688x1242' />
+        <link rel='apple-touch-startup-image' href='/splash_screens/iPhone_11_Pro_Max__iPhone_XS_Max_portrait.png' sizes='1242x2688' />
+        <link rel='apple-touch-startup-image' href='/splash_screens/iPhone_13_mini__iPhone_12_mini__iPhone_11_Pro__iPhone_XS__iPhone_X_landscape.png' sizes='2436x1125' />
+        <link rel='apple-touch-startup-image' href='/splash_screens/iPhone_13_mini__iPhone_12_mini__iPhone_11_Pro__iPhone_XS__iPhone_X_portrait.png' sizes='1125x2436' />
+        <link rel='apple-touch-startup-image' href='/splash_screens/iPhone_14__iPhone_13_Pro__iPhone_13__iPhone_12_Pro__iPhone_12_landscape.png' sizes='2532x1170' />
+        <link rel='apple-touch-startup-image' href='/splash_screens/iPhone_14__iPhone_13_Pro__iPhone_13__iPhone_12_Pro__iPhone_12_portrait.png' sizes='1170x2532' />
+        <link rel='apple-touch-startup-image' href='/splash_screens/iPhone_14_Plus__iPhone_13_Pro_Max__iPhone_12_Pro_Max_landscape.png' sizes='2778x1284' />
+        <link rel='apple-touch-startup-image' href='/splash_screens/iPhone_14_Plus__iPhone_13_Pro_Max__iPhone_12_Pro_Max_portrait.png' sizes='1284x2778' />
+        <link rel='apple-touch-startup-image' href='/splash_screens/iPhone_14_Pro_landscape.png' sizes='2556x1179' />
+        <link rel='apple-touch-startup-image' href='/splash_screens/iPhone_14_Pro_Max_landscape.png' sizes='2796x1290' />
+        <link rel='apple-touch-startup-image' href='/splash_screens/iPhone_14_Pro_Max_portrait.png' sizes='1290x2796' />
+        <link rel='apple-touch-startup-image' href='/splash_screens/iPhone_14_Pro_portrait.png' sizes='1179x2556' />
 
         {/* Schema Markeup - Common */}
         <script type="application/ld+json" 
