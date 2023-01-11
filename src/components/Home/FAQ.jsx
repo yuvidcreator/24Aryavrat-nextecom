865ce74c8b63784cd8b5e94ca1fb3623430bbea8
Yuvraj Limbole <yuvrajl.103@gmail.com>
2023-01-11 12:09:39 +0530

    for --> bug removed

--- src/components/Home/FAQ.jsx
+++ src/components/Home/FAQ.jsx
@@ -23,7 +23,7 @@ const HomeContact = () => {
                             <input type="text" id="full-name" name="full-name" className="w-full bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                         </div> */}
                         <div className="relative mb-4">
-                            <label for="email" className="leading-7 text-sm text-gray-700">Email</label>
+                            <label className="leading-7 text-sm text-gray-700">Email</label>
                             <input type="email" id="email" name="email" className="w-full bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                         </div>
                         <button className="text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg">
